3e163166c6d0ba1ab487eec7ef160dc396341101
Rhiannon99 <78912148+Rhiannon99@users.noreply.github.com>
2024-07-14 18:07:55 +0200

    Update vidplay.py

--- models/vidplay.py
+++ models/vidplay.py
@@ -3,6 +3,7 @@ from typing import Union
 from . import subtitle
 import re
 import base64
+
 async def decode_data(key: str, data: Union[bytearray, str]) -> bytearray:
     key_bytes = bytes(key, 'utf-8')
     s = bytearray(range(256))
@@ -30,22 +31,23 @@ async def decode_data(key: str, data: Union[bytearray, str]) -> bytearray:
             return None
 
     return decoded
+
 async def handle(url) -> dict:
     URL = url.split("?")
     SRC_URL = URL[0]
     SUB_URL = URL[1]
 
     # GET SUB
-    subtitles = {}
     subtitles = await subtitle.vscsubs(SUB_URL)
 
     # DECODE SRC
-    key_req        = await fetch('https://raw.githubusercontent.com/Ciarands/vidsrc-keys/main/keys.json')
-    key1,key2      = key_req.json()
-    decoded_id     = await decode_data(key1, SRC_URL.split('/e/')[-1])
+    key_req = await fetch('https://raw.githubusercontent.com/Ciarands/vidsrc-keys/main/keys.json')
+    keys = key_req.json()
+    key1, key2 = keys[0], keys[1]
+    decoded_id = await decode_data(key1, SRC_URL.split('/e/')[-1])
     encoded_result = await decode_data(key2, decoded_id)
     encoded_base64 = base64.b64encode(encoded_result)
-    key            = encoded_base64.decode('utf-8').replace('/', '_')
+    key = encoded_base64.decode('utf-8').replace('/', '_')
 
     # GET FUTOKEN
     req = await fetch("https://vidplay.online/futoken", {"Referer": url})
@@ -53,14 +55,14 @@ async def handle(url) -> dict:
     data = f"{fu_key},{','.join([str(ord(fu_key[i % len(fu_key)]) + ord(key[i])) for i in range(len(key))])}"
     
     # GET SRC
-    req = await fetch(f"https://vidplay.online/mediainfo/{data}?{SUB_URL}&autostart=true",headers={"Referer": url})
+    req = await fetch(f"https://vidplay.online/mediainfo/{data}?{SUB_URL}&autostart=true", headers={"Referer": url})
     req_data = req.json()
 
     # RETURN IT
-    if type(req_data.get("result")) == dict:
+    if isinstance(req_data.get("result"), dict):
         return {
-            'stream':req_data.get("result").get("sources", [{}])[0].get("file"),
-            'subtitle':subtitles
+            'stream': req_data.get("result").get("sources", [{}])[0].get("file"),
+            'subtitle': subtitles
         }
     else:
         return {}
